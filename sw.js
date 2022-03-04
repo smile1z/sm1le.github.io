7bf02da20b705ec7d4efffdd3810427bda6acb56
smile1z <83390569+smile1z@users.noreply.github.com>
2022-03-04 19:10:26 +0800

    my blog five

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220228.1511';
+const cacheName = 'chirpy-20220304.1854';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
